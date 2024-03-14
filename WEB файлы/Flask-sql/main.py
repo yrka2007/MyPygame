8d658c6a0ef41cbec5cfd8a3a4ff0ff59a715460
yrka2007 <151040924+yrka2007@users.noreply.github.com>
2024-03-14 16:14:09 +0300

    API

--- WEB файлы/Flask-sql/main.py
+++ WEB файлы/Flask-sql/main.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 from data import db_session
 from data.users import User
 from data.news import News
-from forms.user import RegisterForm
+from data.works import Works
+from forms.user import RegisterForm, LoginForm
+from forms.jobs import AddJobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 def main():
@@ -14,11 +19,25 @@ def main():
     app.run(debug=True)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
     news = db_sess.query(News).filter(News.is_private != True)
     return render_template("index.html", news=news)
+
+
+@app.route("/works")
+def works():
+    db_sess = db_session.create_session()
+    works = db_sess.query(Works)
+    return render_template("works.html", works=works)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
@@ -43,6 +62,44 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/addjobs', methods=['GET', 'POST'])
+def addworks():
+    form = AddJobs()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        work = Works()
+        work.title = form.title.data
+        work.content = form.content.data
+        work.user_id = current_user.id
+        db_sess.add(work)
+        db_sess.commit()
+        return redirect('/works')
+    return render_template('addworks.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
 
 if __name__ == '__main__':
     main()
